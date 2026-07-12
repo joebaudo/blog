8f089e1318fd06736b0ec9cd65b8b0863a9d5105
baudo2048 <baudo81@gmail.com>
2026-07-12 20:06:53 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+import "./components/site-header.js";
+import "./components/hero-section.js";
+import "./components/about-section.js";
+import "./components/research-section.js";
+import "./components/skills-section.js";
+import "./components/contact-section.js";
+import "./components/site-footer.js";
